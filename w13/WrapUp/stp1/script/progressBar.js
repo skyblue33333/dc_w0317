b9635516020064fb25d026d63ab531fe34fdd312
skyblue33333 <126849540+skyblue33333@users.noreply.github.com>
2023-06-12 01:46:32 +0900

    12일 중간저장1

---
+++ w13/WrapUp/stp1/script/progressBar.js
@@ -0,0 +1,26 @@
+const indicator =
+  document.querySelector(".indicator");
+const progressTaget =
+  document.querySelector("main");
+
+const progressBar = () => {
+  const progressTagetBCR =
+    progressTaget.getBoundingClientRect();
+  const progress =
+    1 -
+    progressTagetBCR.bottom /
+      progressTagetBCR.height;
+  console.log(progress);
+  if (progress >= 0 && progress <= 1) {
+    indicator.style.cssText =
+      "transform: sclaeX(" + progress + ")";
+  }
+};
+
+// // 이벤트 리스너 추가하는 법1
+// document.onscoll = () => {
+//   progressBar();
+// };
+// 괄호 안에 실행할 함수 구문을 작성 하거나 함수를 갖다 둔다.
+// 이벤트 리스너 추가하는 법2
+document.addEventListener("scroll", progressBar);

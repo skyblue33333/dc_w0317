44ca499ecd66fe1248353f2690166eef2cd2e4d6
skyblue33333 <126849540+skyblue33333@users.noreply.github.com>
2023-06-13 19:20:52 +0900

    중간커밋4

--- w10/WrapUp/stp6/js/script.js
+++
@@ -1,6 +0,0 @@
-let elem = document.getElementById("boxA");
-console.log(elem);
-elem.onclick = () => {
-  // elem.style.backgroundColor = "green"
-  elem.classList.toggle("box--clicked");
-};
---
+++ w10/WrapUp/stp7/js/jsfile.js
@@ -0,0 +1,4 @@
+let aBox = document.getElementById("box1");
+aBox.onclick = () => {
+  aBox.classList.toggle("box--clicked");
+};
